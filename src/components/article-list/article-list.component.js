a7d519812f4c9c8f61280189eda883ceb7054864
ChadH28 <chadhoosain@gmail.com>
2022-04-13 11:52:31 +0200

    added custom stylings

--- src/components/article-list/article-list.component.js
+++ src/components/article-list/article-list.component.js
@@ -1,14 +1,14 @@
 import { Article } from '../article/article.component';
 import React from 'react';
-
+import './article-list.component';
 
 export const ArticleList = props => {
   console.log(props)
   return (
-    <div className='car-list-container'>
-      <div className="card-list">
+    <div className='container'>
+      <div className='row'>
         {
-          props.news.map((article, i)  => <Article key={i} article={article} />)
+          props.news.map((article, i) => <Article key={i} article={article} />)
         }
       </div>
     </div>
