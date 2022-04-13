3d2a329bf69eb373d948b28bc9754ce79b1e9303
ChadH28 <chadhoosain@gmail.com>
2022-04-13 17:06:52 +0200

    made footer component

---
+++ src/components/footer/footer.component.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./footer.styles.css";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="text-center">
+          © 2022 By Daily Sunrise. Made by Chad,All Ts and Cs apply
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
